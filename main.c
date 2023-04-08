b7ba6abae253e4f015dab313f720b358023f4fdf
Dongmin Lee <admin@ldmsys.net>
2023-04-09 05:07:01 +0900

    feat: add upscaling

--- main.c
+++ main.c
@@ -6,11 +6,33 @@
 #include "font.h"
 #define RGB(r,g,b) r|g<<8|b<<16
 
+
+void upScaleGreyImage(unsigned char* newimg, unsigned char* img, int imgX, int imgY, int factor) {
+    int gxl, gxr, gyu, gyd;
+    memset(newimg, 0, imgX*imgY*factor*factor);
+    for(int i=0;i<imgX;i++) {
+        for(int j=0;j<imgY;j++) {
+            newimg[i*factor*(imgY*factor)+j*factor] = img[i*imgY+j]; // newimg[i*factor][j*factor] = img[i][j];
+            
+            gxl = i > 0 ? (i*factor - (factor-1)/2) : 0;
+            gxr = (i == (imgX - 1)) ? imgX*factor : (i*factor + (factor-1)/2);
+            gyu = j > 0 ? (j*factor - (factor-1)/2) : 0;
+            gyd = (j == (imgY - 1)) ? imgY*factor : (j*factor + (factor-1)/2);
+            
+            for(int k=0;k<=gxr-gxl;k++) {
+                for(int l=0;l<=gyd-gyu;l++) {
+                    newimg[(gxl+k)*(imgY*factor)+gyu+l] = img[i*imgY+j];
+                }
+            }
+        }
+    }
+}
+
 void printGreyImage(int* buf, int bufX, unsigned char* img, int imgX, int imgY, int sposX, int sposY) {
     int tmpi;
     for(int i=0;i<imgX;i++) for(int j=0;j<imgY;j++) {
         tmpi = 0xdb000000;
-        for(int k=0;k<3;k++) tmpi += (unsigned char)img[i*imgX+j]<<(8*k);
+        for(int k=0;k<3;k++) tmpi |= (unsigned char)img[i*imgX+j]<<(8*k);
         buf[(sposX+i)*bufX+sposY+j] = tmpi;
     }
 }
@@ -38,6 +60,8 @@ int main(int argc,char** argv) {
     FILE* caman;
     int tmpi, *buf;
     unsigned char img[256][256];
+    unsigned char upscaled[768][768];
+    memset(upscaled, 0xff, 768*768);
     caman = fopen("./caman", "r");
     if(!caman) {
         perror("fopen");
@@ -48,6 +72,8 @@ int main(int argc,char** argv) {
     buf=(int*)malloc(1024*768*4);
     rfbScreenInfoPtr server=rfbGetScreen(&argc,argv,1024,768,8,3,4);
     fillColor(buf, 768, 0, 0, 1024, 768, RGB(0, 0, 0));
+    upScaleGreyImage(upscaled, img, 256, 256, 3);
+    printGreyImage(buf, 1024, upscaled, 256*3, 256*3, 0, 0);
     printGreyImage(buf, 1024, img, 256, 256, 256, 384);
     printString(buf, 1024, 10, 10, "Make better with [\"passion\", \"code\", \"hard work\"]", RGB(255, 255, 255));
     printString(buf, 1024, 18, 10, "+1 (888) 318-1430", RGB(0, 255, 0));
